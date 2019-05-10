712cabad563ef2d5e8955ddf876012477c9c6fce
Ludvig Jordet <ludvig.jordet@gmail.com>
2019-05-10 07:26:24 +0200

    Load contact info from API
    Contact info (both header/footer and contact page) is now
    loaded via the Låfte kontaktinfo API

---
+++ js/contactInfo.js
@@ -0,0 +1,1 @@
+export let contactInfoRequest = fetch('https://laafte.samfundet.no/api/kontaktinfo/').then(response => response.json());
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-document.getElementById("picture").onclick = () => {
-    alert("HELLO!");
-}
+import {contactInfoRequest} from "./contactInfo.js";
+
+document.addEventListener("DOMContentLoaded", event => {
+    let leaderInfos = document.querySelectorAll(".leader-info");
+    contactInfoRequest.then(data => {
+        // API does in fact support multiple leaders, pick the first one and keep minding our own business
+        let sjef = data.contacts.filter(contact => contact.role === "orkestersjef")[0];
+        leaderInfos.forEach(element => {
+            element.textContent = `Orkestersjef: ${sjef.name} | laafte-sjef@samfundet.no | ${sjef.phone}`;
+        })
+    })
+})
